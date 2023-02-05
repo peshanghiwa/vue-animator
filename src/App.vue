5d12a51595bba19d2a6d04d06db19e5e2b8f35f5
Peshang Hiwa <peshanghiwa0@gmail.com>
2023-02-05 10:48:46 +0300

    fixed animation sample on app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,49 @@ import { Animator } from ".";
 </script>
 
 <template>
-  <Animator
-    :to="[
-      {
-        transform: 'translateX(100px)',
-        backgroundColor: 'red',
-      },
-    ]"
-  >
-    <div>test</div>
-  </Animator>
+  <div class="test-container">
+    <Animator
+      :delay="1000"
+      :to="[
+        {
+          scale: 2,
+          rotate: '0deg',
+          borderRadius: '20%',
+        },
+        {
+          scale: 2,
+          rotate: '270deg',
+          borderRadius: '50%',
+        },
+        {
+          scale: 1,
+          rotate: '270deg',
+          borderRadius: '50%',
+        },
+        {
+          scale: 1,
+          rotate: '0deg',
+          borderRadius: '20%',
+        },
+      ]"
+    >
+      <div class="test"></div>
+    </Animator>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.test-container {
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.test {
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+}
+</style>
